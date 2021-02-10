c24427ef1567a40ad87d3c02c9a14165b15ba3c7
Ben Johnston <bjohnston@dujuan.cosmic.ucar.edu>
2021-02-10 20:46:48 +0000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name="mysci",
+   version="1.0.0",
+   description="A sample package",
+   author="Ben Johnston",
+   author_email="bjohnston@ucar.edu",
+   package=["mysci"],
+   install_requires=[])
